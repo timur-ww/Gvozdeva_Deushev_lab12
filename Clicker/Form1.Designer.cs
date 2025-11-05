7df57697cf7d7b0f68a3219de7b5a857bdbe5ecd
timurqqe <timurkkq@gmail.com>
2025-11-05 21:20:35 +0300

    Add files via upload

---
+++ Clicker/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Clicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.labelTime = new System.Windows.Forms.Label();
+            this.labelClick = new System.Windows.Forms.Label();
+            this.buttonClick = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelTime.Location = new System.Drawing.Point(6, 6);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(159, 31);
+            this.labelTime.TabIndex = 0;
+            this.labelTime.Text = "Время: 0,0";
+            // 
+            // labelClick
+            // 
+            this.labelClick.AutoSize = true;
+            this.labelClick.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelClick.Location = new System.Drawing.Point(6, 63);
+            this.labelClick.Name = "labelClick";
+            this.labelClick.Size = new System.Drawing.Size(165, 31);
+            this.labelClick.TabIndex = 1;
+            this.labelClick.Text = "Нажатий: 0";
+            // 
+            // buttonClick
+            // 
+            this.buttonClick.Image = ((System.Drawing.Image)(resources.GetObject("buttonClick.Image")));
+            this.buttonClick.Location = new System.Drawing.Point(12, 120);
+            this.buttonClick.Name = "buttonClick";
+            this.buttonClick.Size = new System.Drawing.Size(255, 234);
+            this.buttonClick.TabIndex = 2;
+            this.buttonClick.UseVisualStyleBackColor = true;
+            this.buttonClick.Click += new System.EventHandler(this.buttonClick_Click);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(282, 279);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(100, 75);
+            this.buttonReset.TabIndex = 3;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 360);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonClick);
+            this.Controls.Add(this.labelClick);
+            this.Controls.Add(this.labelTime);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Clicker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.Label labelClick;
+        private System.Windows.Forms.Button buttonClick;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Clicker/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clicker
+{
+    public partial class Form1 : Form
+    {
+        private int clicks;
+        private int milliseconds, seconds; 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void UpdateLabel()
+        {
+            labelTime.Text = $"Время: {seconds},{milliseconds}";
+            labelClick.Text = $"Нажатий: {clicks}";
+        }
+          
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            milliseconds = seconds = 0;
+            clicks = 0;
+            UpdateLabel();
+        }
+
+        private void buttonClick_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            clicks++;
+            UpdateLabel();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            milliseconds += 15;
+            if (milliseconds >= 1000)
+            {
+                milliseconds = 0;
+                seconds++;
+            }
+            UpdateLabel();
+        }
+    }
+}
